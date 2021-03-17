9ea7a89f190c05688ef6125985ad9323e94132fc
Pedro-ss <pedross.tool@gmail.com>
2021-03-17 14:13:50 -0300

    adicionando lógica para nickname

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.aboutme
 
+import android.content.Context
+import android.hardware.input.InputManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener{
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View){
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameText = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameText.text = editText.text
+        editText.visibility = View.GONE
+        nicknameText.visibility = view.visibility
+        view.visibility = View.GONE
+
+        //hide keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken , 0)
     }
 }
